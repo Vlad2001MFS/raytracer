f84a677696b6d54380ffcc5d493e4d7b4027875a
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-09-03 21:05:16 +0300

    improve Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -8,7 +8,8 @@ use std::{
     }
 };
 
-pub struct Vec3(f64, f64, f64);
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3(pub f64, pub f64, pub f64);
 
 impl Add for Vec3 {
     type Output = Vec3;
@@ -82,6 +83,14 @@ impl Mul<f64> for Vec3 {
     }
 }
 
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3(self*rhs.0, self*rhs.1, self*rhs.2)
+    }
+}
+
 impl MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, rhs: f64) {
         self.0 *= rhs;
@@ -95,6 +104,14 @@ impl Div<f64> for Vec3 {
 
     fn div(self, rhs: f64) -> Self::Output {
         Vec3(self.0 / rhs, self.1 / rhs, self.2 / rhs)
+    }
+}
+
+impl Div<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn div(self, rhs: Vec3) -> Self::Output {
+        Vec3(self / rhs.0, self / rhs.1, self / rhs.2)
     }
 }
 
@@ -121,5 +138,21 @@ impl Vec3 {
 
     pub fn length(&self) -> f64 {
         self.length_sq().sqrt()
+    }
+
+    pub fn dot(&self, rhs: Vec3) -> f64 {
+        self.0*rhs.0 + self.1*rhs.1 + self.2*rhs.2
+    }
+
+    pub fn cross(&self, rhs: Vec3) -> Vec3 {
+        Vec3(
+            self.1*rhs.2 - self.2*rhs.1,
+            self.2*rhs.0 - self.0*rhs.2,
+            self.0*rhs.1 - self.1*rhs.0,
+        )
+    }
+    
+    pub fn normalized(self) -> Vec3 {
+        self / self.length()
     }
 }
